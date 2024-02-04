753dab3aff75c6647ac824d28a571281f5a665f8
Igor <igor.chudesny1991@gmail.com>
2024-02-04 17:09:49 +0100

    'testcommit'

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+let webpackMode = process.env.NODE_ENV ? 'develop' : 'production'
+module.exports = {
+	mode: webpackMode,
+	entry: {
+		main: './source/js/app.js',
+		faq: './source/js/faq.js',
+	},
+	output: {
+		filename: '[name].bundle.js',
+	}
+};
+
+
